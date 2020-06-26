e31795bf26203cb03120db0a6641d46e4bd9ad14
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-26 15:31:46 -0500

    3.1

---
+++ tests/3_test.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+sys.path.append('./tests')
+
+from utils import *
+
+class VariableTest(unittest.TestCase):
+    def test_add_no_numbers(self):
+        self.assertEqual(getLastCommand(), "python3 blackjack.py", "entered correct last command")
+        self.assertEqual(getCwd(), "/home/strove/project", "entered in correct directory")
+
+if __name__ == '__main__':
+    unittest.main()
